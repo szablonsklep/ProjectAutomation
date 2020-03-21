6f0b8cfe9a5243872776d5cc46586f540a87901b
Grzegorz Rusin <grzegorz.rusin@szablonsklep.pl>
2020-03-21 20:08:56 +0100

    minor fix

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -4,15 +4,23 @@
 
 function create() {
 
-  python Desktop/Python/test01/skiny/create.py $1
-  cd Desktop/Python/$1
-  
-  git init
-  git remote add origin git@github.com:{your_git_username}/$1.git
-  touch README.md
-  git add .
-  git commit -m "Initial commit"
-  git push -u origin master
-  #open code editor with project
-  code .
+  python3 {path to your python file} $1
+
+  #check if folder was created
+  if [[ -d {project_folder} ]]
+  then
+    cd Desktop/Python/$1
+
+    #initialize git repo
+    git init
+    git remote add origin git@github.com:{your_git_username}/$1.git
+    touch README.md
+    git add .
+    git commit -m "Initial commit"
+    git push -u origin master
+    #open code editor (visual studio) with project
+    code .
+  else
+    echo "Your $1 folder does not exist"
+  fi
 }
--- create.py
+++ create.py
@@ -13,10 +13,13 @@ def create():
     folder_name = str(sys.argv[1])
     #create folder with project
     os.makedirs(path + str(folder_name))
-    #connect to git 
+    #connect to git as user
     g = Github(str(access_token)).get_user()
+    #or connect to git as organiztion
+    #g = Github(str(access_token)).get_organization('orgtanizoation_name')
     #create repo (private is optional)
     repo = g.create_repo(folder_name, private=True)
+    print(repo)
 
     #print 
     print("Succesfully created repository {}".format(folder_name))
